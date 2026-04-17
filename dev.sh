279c7c4afd0b24a30966d2c3a05068c6ca4c6640
Jon Wilson <jon.wilson@dhcs.ca.gov>
2026-04-17 14:54:33 -0700

    simplify dependencies

--- dev.sh
+++ dev.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-env $(cat .env | xargs) go run github.com/air-verse/air
+env $(cat .env | xargs) go run github.com/air-verse/air@latest
--- tools.go
+++
@@ -1,7 +0,0 @@
-//go:build tools
-
-package function
-
-import (
-	_ "github.com/air-verse/air"
-)
